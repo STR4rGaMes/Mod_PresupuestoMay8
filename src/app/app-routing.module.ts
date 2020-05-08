f2d3c50da1ff9fa841def87aee8fee079300daf5
Carlos Gonzales <gonzales597@gmail.com>
2020-05-08 13:06:47 -0500

    ModulosRouting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,31 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
+const moduleRoutes: Routes = [
+
+  {
+    path:'bud',
+    loadChildren:() => import('./Modules/but/but.module').then(m => m.ButModule)
+  },
+  {
+    path:'log',
+    loadChildren:() => import('./Modules/log/log.module').then(m => m.LogModule)
+  },
+  {
+    path:'rep',
+    loadChildren:() => import('./Modules/rep/rep.module').then(m=> m.RepModule)
+  }
+  {
+    path:'sys',
+    loadChildren:() => import('./Modules/sys/sys.module').then(m=> m.SysModule)
+  }
+  {
+    path:'tre',
+    loadChildren:() => import('./Modules/tre/tre.module').then(m=> m.TreModule)
+  }
+]
+
+
 const routes: Routes = [];
 
 @NgModule({
